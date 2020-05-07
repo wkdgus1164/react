c13ae645116566f8a641b08058017d0bc60a402e
Han Jang Hyun <wkdgus1164@gmail.com>
2020-05-08 00:19:04 +0900

    fullstack doing
    fullstack doing

---
+++ react-fullstack/client/servers/server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const port = process.env.PORT || 3001
+
+app.use(bodyParser.json())
+app.use('/api', (req, res) => res.json({ username: "han" }))
+app.listen(port, () => console.log(`running on ${port}`))
